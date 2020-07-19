9eb556fa0dc243b17703e4567639f0fadf8c3bf3
srknskr <seker.serkan.3@gmail.com>
2020-07-19 13:01:58 +0300

    Proje dosyası ekle.

---
+++ Weather/Weather/Model/Onboarding.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Weather.Model
+{
+    public class Onboarding
+    {
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
